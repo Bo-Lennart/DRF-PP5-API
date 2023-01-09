b1be4014fb6f8bd5b98e6bcacf1c3ea1cdf92831
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2023-01-09 11:28:04 +0000

    add like list, like serializer

--- drf_pp5_api/urls.py
+++ drf_pp5_api/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('', include('profiles.urls')),
-    path('', include('blogpost.urls'))
+    path('', include('blogpost.urls')),
+    path('', include('like.urls'))
 ]
--- like/views.py
+++ like/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_pp5_api.permissions import IsOwnerOrReadOnly
+from like.models import Like
+from like.serializers import LikeSerializer
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    get like or delete if owner of the like
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
