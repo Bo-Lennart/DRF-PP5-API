cfefeb023f83373a9f5fd528416009322e47f4d5
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2023-01-10 07:52:10 +0000

    add bookmark and like ID to postserializer

--- blogpost/serializers.py
+++ blogpost/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from blogpost.models import BlogPost
+from like.models import Like
+from bookmark.models import Bookmark
 
 
 class BlogPostSerializer(serializers.ModelSerializer):
@@ -7,15 +9,35 @@ class BlogPostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
+    bookmark_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return like.id if like else None
+        return None
+
+    def get_bookmark_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            bookmark = Bookmark.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return bookmark.id if bookmark else None
+        return None
 
     class Meta:
         model = BlogPost
         fields = [
             'id', 'category', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image'
+            'title', 'content', 'image', 'like_id', 'bookmark_id'
         ]
