92b9fec496a83eea37fa61ce84e067987920fe97
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2023-01-10 10:58:32 +0000

    update allowed hosts

--- drf_pp5_api/settings.py
+++ drf_pp5_api/settings.py
@@ -62,7 +62,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost','pp5-content-page-api.herokuapp.com']
+ALLOWED_HOSTS = [
+    'pp5-content-page-api.herokuapp.com',
+    'localhost',
+    ]
 
 
 # Application definition
