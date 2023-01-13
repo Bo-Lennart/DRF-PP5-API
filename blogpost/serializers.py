be98d111e3f0d1959d6238068d2a263c06a7f180
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2023-01-13 14:04:26 +0000

    update blogpost serializer to show bookmarks count and likes count

--- blogpost/serializers.py
+++ blogpost/serializers.py
@@ -11,27 +11,39 @@ class BlogPostSerializer(serializers.ModelSerializer):
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     like_id = serializers.SerializerMethodField()
     bookmark_id = serializers.SerializerMethodField()
+    likes_count = serializers.ReadOnlyField()
+    bookmarks_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
     def get_like_id(self, obj):
+        """
+        Return & calculate total number
+        of likes on post view.
+        """
         user = self.context['request'].user
+
         if user.is_authenticated:
             like = Like.objects.filter(
-                owner=user, post=obj
+                owner=user,
+                post=obj
             ).first()
             return like.id if like else None
+
         return None
 
     def get_bookmark_id(self, obj):
+
         user = self.context['request'].user
+
         if user.is_authenticated:
             bookmark = Bookmark.objects.filter(
                 owner=user, post=obj
             ).first()
             return bookmark.id if bookmark else None
+
         return None
 
     class Meta:
@@ -39,5 +51,6 @@ class BlogPostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'category', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image', 'like_id', 'bookmark_id'
+            'title', 'content', 'image', 'like_id', 'bookmark_id', 
+            'likes_count', 'bookmarks_count'
         ]
