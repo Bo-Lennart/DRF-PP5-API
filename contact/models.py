232bf385de0c59645c9ecb9de163a9eb7f2f0377
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2023-01-17 12:35:46 +0000

    add contact model to models py

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.model):
+    first_name: models.Charfield(max_length=50)
+    last_name: models.Charfield(max_length=50)
+    email_adress: models.Charfield(max_length=50)
+    message: models.Charfield(max_length=50)
+    created: models.DateTimeField(auto_now_add=True)
+
+    
