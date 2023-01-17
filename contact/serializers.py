e4024e1697acbcdae440236555820cd4dffcf663
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2023-01-17 12:55:48 +0000

    create serializers fields for contact form

--- contact/serializers.py
+++ contact/serializers.py
@@ -1,4 +1,10 @@
 from rest_framework import serializers
 from .models import Contact
 
+
 class ContactSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = [
+            'first_name', 'last_name', 'email_adress', 'message', 'created'
+        ]
