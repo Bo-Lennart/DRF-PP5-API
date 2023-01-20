36bbca1174d3a983a5c38b2cb470847ce7675c85
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2023-01-20 20:33:08 +0000

    final commit

--- drf_pp5_api/settings.py
+++ drf_pp5_api/settings.py
@@ -61,7 +61,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'pp5-api-bo.herokuapp.com']
 
@@ -193,7 +193,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = ''
 STATIC_URL = '/static/'
 
 # Default primary key field type
